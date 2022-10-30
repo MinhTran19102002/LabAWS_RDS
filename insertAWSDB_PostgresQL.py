af19b13a8a1304a319446b8e2e6a980d762fca15
Minh Tran <20110677@student.hcmute.edu.vn>
2022-10-30 18:48:00 +0700

    first commit

--- insertAWSDB_PostgresQL.py
+++ insertAWSDB_PostgresQL.py
@@ -3,7 +3,8 @@ import psycopg2
 
 
 #Tạo kết nối với RDS PostgreSQL và thêm dữ liệu vào
-conn = psycopg2.connect(database='covid19_postgres', user='postgres', password='12345678', host='database-postgresql.ctnk8ti4crcr.us-east-1.rds.amazonaws.com', port='5432')
+conn = psycopg2.connect(database='covid19_postgres', user='postgres', password='lab-password', 
+                        host='database-postgresql.c6svwtcroyou.us-east-1.rds.amazonaws.com', port='5432')
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
